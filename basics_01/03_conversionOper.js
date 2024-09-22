82a7b0f77bce4ca4218abf11fbcd0acf91c96809
yashashree12-12 <yashashree12042005@gmail.com>
2024-09-22 19:04:29 +0000

    conversionOperation

--- basics_01/03_conversionOper.js
+++ basics_01/03_conversionOper.js
@@ -1,4 +1,4 @@
-let score = "40"
+/*let score = "40"
 console.log(typeof score);
 console.log(typeof (score));
 
@@ -20,3 +20,40 @@ let someNumber = 444
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//-----------------------operations----------------------
+*/
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " yashashree"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+console.log((3+4)*5%3);
+
+
+console.log(+true);//1
+console.log(+"");//0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); 
+
