a7ba082cf7836764a598f3e5673268c291d3ea5c
yashashree12-12 <yashashree12042005@gmail.com>
2024-09-22 19:43:39 +0000

    data-summary

---
+++ basics_01/datatype-summary.js
@@ -0,0 +1,38 @@
+// primitive datatype
+
+// 7 types : String, Number, Boolean, null, undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);//false
+
+const bigNumber = 33456788765435677654n
+
+//Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "yashashree",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof heros); //object 
+console.log(typeof bigNumber); //undefined
+console.log(typeof outsideTemp); //object
+console.log(typeof scoreValue); //number
+
