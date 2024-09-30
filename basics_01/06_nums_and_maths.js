886c31008a41988dcde910c502576f6f807bf9cc
yashashree12-12 <yashashree12042005@gmail.com>
2024-09-30 16:07:08 +0000

    number and maths

---
+++ basics_01/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance); 
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(4));
+
+const otherNumber = 23.89587
+
+console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+// +++++++++++++++++++++++++++maths=========================
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.2));//round upto top
+console.log(Math.floor(4.4));//ROUND UPTO BOTTOM VALUE
+console.log(Math.max(4,3,6,8));//finds max
+console.log(Math.min(4,3,6,8));//finds min
+console.log(Math.random()); //random value between 0-1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+/**/
+console.log(Math.floor(Math.random() * (max-min+1)) + min )
