0781812c8ab60c13100a7ccbb90a4b14f742694a
yashashree12-12 <yashashree12042005@gmail.com>
2024-10-01 14:03:37 +0000

    datesInJs

---
+++ basics_01/07_datesinJs.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0, 23)
+let myCreateDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreateDate.toLocaleString()) ;
